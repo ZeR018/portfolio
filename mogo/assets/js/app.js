6a4ee0ce55f00b8059d50510993205cd657f9aed
ZeR018 <62484304+ZeR018@users.noreply.github.com>
2020-11-27 23:52:15 +0300

    Mogo HTML
    В этом проекте я тренировал вёрстку.

---
+++ mogo/assets/js/app.js
@@ -0,0 +1,79 @@
+$(function() {
+
+  let header = $("#header"),
+      introH = $("#intro").innerHeight(),
+      scrollOffset = $(window).scrollTop();
+
+
+      // Fixed Header
+
+      checkScroll(scrollOffset);
+
+  $(window).on("scroll", function() {
+    checkScroll(scrollOffset);
+  });
+
+  function checkScroll(scrollOffset) {
+    scrollOffset = $(this).scrollTop();
+
+    if( scrollOffset >= introH ) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  }
+
+
+  // Smooth scroll
+  $('[data-scroll]').on('click', function(event) {
+    event.preventDefault();
+
+    let $this = $(this),
+      blockId = $(this).data('scroll'),
+      blockOffset = $(blockId).offset().top;
+
+    $('#nav a').removeClass('active');
+    $this.addClass('active');
+
+      $('html, body').animate({
+        scrollTop: blockOffset
+      }, 500);
+
+
+  });
+
+  // Menu nav toggle
+
+  $('#nav_toggle').on('click', function(event) {
+    event.preventDefault();
+
+    $(this).toggleClass('active');
+    $('#nav').toggleClass('active');
+  });
+
+  // Collapse
+
+  $('[data-collapse]').on('click', function(event) {
+    event.preventDefault();
+
+    let $this = $(this),
+      blockId = $this.data('collapse');
+
+    $this.toggleClass('active');
+  });
+
+// Slider
+
+  $('[data-slider]').slick({
+    infinite: true,
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+
+  });
+
+
+
+
+
+});
